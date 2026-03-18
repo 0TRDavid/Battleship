f7ba9b1f800fee3974a77046e793614ae6587123
Jules&David <test>
2026-03-18 09:27:50 +0100

    first commit

---
+++ Battleship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Battleship
+//
+//  Created by David Truong on 17/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
